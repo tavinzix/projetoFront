a84478dee83d84c59d88db9f097ca88b55534bc7
Otávio Fagundes <62486331+tavinzix@users.noreply.github.com>
2025-04-23 16:09:04 -0300

    slider produtos

--- js/js.js
+++ js/js.js
@@ -4,3 +4,15 @@ const menu = document.getElementById('menu-link');
 hamb.addEventListener('click', () => {
   menu.classList.toggle('active');
 });
+
+const carrossel = document.querySelector('.carrossel');
+const btnEsquerda = document.querySelector('.setaEsquerda');
+const btnDireita = document.querySelector('.setaDireita');
+
+btnEsquerda.addEventListener('click', () => {
+  carrossel.scrollBy({ left: -220, behavior: 'smooth' });
+});
+
+btnDireita.addEventListener('click', () => {
+  carrossel.scrollBy({ left: 220, behavior: 'smooth' });
+});
