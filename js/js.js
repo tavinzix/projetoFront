179c135f2c21c4bc07352f58ef0cd8b6e11f6d93
otavio <otaviofariafagundes1@gmail.com>
2025-05-06 17:44:49 -0300

    perfil do usuario

--- js/js.js
+++ js/js.js
@@ -142,3 +142,29 @@ function mostrarCategoria(categoria) {
   document.getElementById(categoria).classList.remove('oculto');
   document.querySelector(`.aba[onclick="mostrarCategoria('${categoria}')"]`).classList.add('ativa');
 }
+
+/*PERFIL DO USUARIO*/
+
+function filtrarPedidos(filtro) {
+  const pedidos = document.querySelectorAll('.lista-pedidos .pedido');
+  const abas = document.querySelectorAll('.filtros-pedidos .aba');
+  if (pedidos && abas) {
+    abas.forEach(aba => aba.classList.remove('ativa'));
+
+    const abaSelecionada = document.querySelector(`.filtros-pedidos .aba[onclick="filtrarPedidos('${filtro}')"]`);
+    if (abaSelecionada) {
+      abaSelecionada.classList.add('ativa');
+    }
+
+    pedidos.forEach(pedido => {
+      const statusPedido = pedido.querySelector('.status');
+      const status = statusPedido?.classList[1];
+
+      if (filtro === 'todos' || status === filtro) {
+        pedido.style.display = 'flex';
+      } else {
+        pedido.style.display = 'none';
+      }
+    });
+  }
+}
